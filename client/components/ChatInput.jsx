c8a5e26552de1da1a592b43df063735cae7e721b
Derek Lui <luiderek@outlook.com>
2022-05-31 01:21:51 +0000

    creating the input component

---
+++ client/components/ChatInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ChatInput(props) {
+  return (
+    <form>
+      <input
+      className='chat-input'
+      placeholder='Message #room'>
+      </input>
+    </form>
+  );
+}
