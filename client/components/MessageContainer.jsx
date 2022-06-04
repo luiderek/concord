7db396539a8a26d61216009dd4407dcff786774b
Derek Lui <luiderek@outlook.com>
2022-06-04 03:10:26 +0000

    adding library to autoscroll bottom

--- client/components/MessageContainer.jsx
+++ client/components/MessageContainer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Message from './Message';
 import AppContext from '../lib/app-context';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 export default function MessageContainer(props) {
   return (
     <div className='message-container'>
-      <AppContext.Consumer>
-        {context => {
-          return context.messages.map(msg => (
+      <ScrollToBottom className='message-container'>
+        <AppContext.Consumer>
+          {context => {
+            return context.messages.map(msg => (
               <Message key={msg.message_id}
                 time={new Date(msg.post_time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
                 name={msg.username}
@@ -15,10 +17,11 @@ export default function MessageContainer(props) {
                 msgID={msg.message_id}
                 edited={msg.edited}
                 sameUser={msg.username === context.user.username} />
-          ));
-        }
-        }
-      </AppContext.Consumer>
+            ));
+          }
+          }
+        </AppContext.Consumer>
+      </ScrollToBottom>
     </div>
   );
 }
