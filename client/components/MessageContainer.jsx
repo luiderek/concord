cd7231b0e6245bbb2b204ff600bbd2e1d3885fc4
Derek Lui <luiderek@outlook.com>
2022-06-04 03:38:48 +0000

    fixing triple scrollbar bug

--- client/components/MessageContainer.jsx
+++ client/components/MessageContainer.jsx
@@ -5,24 +5,22 @@ import ScrollToBottom from 'react-scroll-to-bottom';
 
 export default function MessageContainer(props) {
   return (
-    <div className='message-container'>
-      <ScrollToBottom className='message-container'>
-        <AppContext.Consumer>
-          {context => {
-            return context.messages.map(msg => (
-              <Message key={msg.message_id}
-                time={new Date(msg.post_time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-                name={msg.username}
-                content={msg.content}
-                msgID={msg.message_id}
-                edited={msg.edited}
-                sameUser={msg.username === context.user.username} />
-            ));
-          }
-          }
-        </AppContext.Consumer>
-      </ScrollToBottom>
-    </div>
+    <ScrollToBottom className='message-container'>
+      <AppContext.Consumer>
+        {context => {
+          return context.messages.map(msg => (
+            <Message key={msg.message_id}
+              time={new Date(msg.post_time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+              name={msg.username}
+              content={msg.content}
+              msgID={msg.message_id}
+              edited={msg.edited}
+              sameUser={msg.username === context.user.username} />
+          ));
+        }
+        }
+      </AppContext.Consumer>
+    </ScrollToBottom >
   );
 }
 
