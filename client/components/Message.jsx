63530ebbc67bbf1c7b1f4522a63c28970dfff16d
Derek Lui <luiderek@outlook.com>
2022-06-04 07:57:24 +0000

    layout css for pencil-trash icon

--- client/components/Message.jsx
+++ client/components/Message.jsx
@@ -5,29 +5,34 @@ import EditMessageInput from './EditMessageInput';
 export default function Message(props) {
   return (
     <>
-    <div className='message'>
-      <span>
-        {props.time}
-      </span>
-      <span>
-        {props.name}
-      </span>
-      <span>
-        {props.content}
-        {/* The edited-marker needs to be on it's own span for copy-paste.
+      <div className='message'>
+        <span>
+          <span>
+            {props.time}
+          </span>
+          <span>
+            {props.name}
+          </span>
+          <span>
+            {props.content}
+            {/* The edited-marker needs to be on it's own span for copy-paste.
           Turns out, user-select:none also prevents span bleed.
           Might be a useful trick in the future for this stuff. */}
-        {props.edited === true &&
-          <span className='edited-marker'>
-            (edited)
+            {props.edited === true &&
+              <span className='edited-marker'>
+                (edited)
+              </span>
+            }
+          </span>
+        </span>
+        {props.sameUser &&
+          <span className='icon-wrapper'>
+            <i className="fa-solid fa-pencil" />
+            <DeleteConfirmModal {...props} />
           </span>
         }
-      </span>
-
-      {props.sameUser ? <i className="fa-solid fa-pencil"></i> : '' }
-      {props.sameUser ? <DeleteConfirmModal {...props} /> : '' }
-    </div>
-    { true === false && <EditMessageInput /> }
+      </div>
+      {true === false && <EditMessageInput />}
     </>
   );
 }
