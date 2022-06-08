5729196ae0f9f4bd4bec1fa79dccdc9ea9b7a47c
Derek Lui <luiderek@outlook.com>
2022-06-08 08:01:12 +0000

    remove comments, form blurs on submit

--- client/components/ChatInput.jsx
+++ client/components/ChatInput.jsx
@@ -8,16 +8,8 @@ export default function ChatInput(props) {
   // Object { message_id: 9, content: "testing", post_time: "2022-06-08T05:03:39.427Z", user_id: 3, room_id: 1, edited: false, username: "aaaaah" }
 
   const handleFocus = e => {
-    // Open line.
-    // My first problem is that to slip something into the message component and for it to maintain it's order,
-    // it needs to have a message_id, which I can only really get from hitting the SQL at some point.
-    // is it ordered by key?
-    // is it possible to generate a key?
-    //
-    // console.log('handle focus:', e);
     if (!currentlyBroadcasting) {
       toggleBroadcast(true);
-      // console.log('props', props);
       const rttMessage = {
         content: '',
         user_id: props.user.userID,
@@ -43,15 +35,12 @@ export default function ChatInput(props) {
         message_id: broadcastID,
         post_time: new Date()
       };
-      // console.log('handlechange called:', e.target.value);
       socket.emit('rtt update', rttMessage);
     }
   };
 
   const handleBlur = e => {
     if (e.target.value === '') {
-      // console.log('input is blurred with empty form');
-      // Close line would be appropriate here.
       if (currentlyBroadcasting) {
         toggleBroadcast(false);
         socket.emit('rtt close', broadcastID);
@@ -64,13 +53,11 @@ export default function ChatInput(props) {
     e.preventDefault();
     const { user, roomID } = props;
 
+    e.target[0].blur();
     toggleBroadcast(false);
     socket.emit('rtt close', broadcastID);
     changeBroadcastID(Math.random());
 
-    // console.log(props);
-    // Bad SQL returns from an empty context value was breaking the website.
-    // My error handling can use improvement so it breaks things less.
     if (e.target.elements[0].value === '') {
       return;
     }
@@ -81,7 +68,6 @@ export default function ChatInput(props) {
         'Content-Type': 'application/json',
         'x-access-token': window.localStorage.getItem('react-context-jwt')
       },
-      // eventually read from context state current room.
       body: JSON.stringify({
         room: roomID,
         userID: user.userId,
@@ -89,7 +75,6 @@ export default function ChatInput(props) {
       })
     }).then(res => res.json())
       .then(data => {
-        // console.log('submitted data', data);
         socket.emit('message submit', data);
         e.target.reset(); // Clear form.
       })
