d693b04f1c2dc952e5c697cee3c4f1a3a2174864
Derek Lui <luiderek@outlook.com>
2022-06-10 17:17:00 +0000

    move user-sign-out from Home to RoomSidebar

--- client/components/RoomSidebar.jsx
+++ client/components/RoomSidebar.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import Room from './Room';
+import Button from 'react-bootstrap/Button';
 import CreateRoomModal from './CreateRoomModal';
 import ChangeServerModal from './ChangeServerModal';
 
 export default function RoomSidebar(props) {
   return (
-    <div className="room-sidebar">
-      <ChangeServerModal
-        serverName={props.serverName}
-        handleServerChange={props.handleServerChange}
-      />
-      <CreateRoomModal serverID={props.serverID} />
-      {props.rooms.map(msg => (
-        <Room
-          key={msg.room_id}
-          name={msg.room_name}
+    <>
+      <div className="room-sidebar">
+        <ChangeServerModal
           serverName={props.serverName}
-          isActive={msg.room_name === props.roomName}
+          handleServerChange={props.handleServerChange}
         />
-      ))}
-    </div>
+        <CreateRoomModal serverID={props.serverID} />
+        {props.rooms.map(msg => (
+          <Room
+            key={msg.room_id}
+            name={msg.room_name}
+            serverName={props.serverName}
+            isActive={msg.room_name === props.roomName}
+          />
+        ))}
+      </div>
+      <div className="user-sign-out">
+        <span>{props.user.username}</span>
+        <Button onClick={props.handleSignOut}>sign out</Button>
+      </div>
+    </>
   );
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import MessageContainer from '../components/MessageContainer';
 import AppContext from '../lib/app-context';
 import Redirect from '../components/redirect';
-import { Button } from 'react-bootstrap';
 import RoomSidebar from '../components/RoomSidebar';
 import ChatInput from '../components/ChatInput';
 
 export default class Home extends React.Component {
   render() {
-    const { user, handleSignOut } = this.context;
     if (!this.context.user) return <Redirect to="auth" />;
 
     return (
@@ -16,10 +14,6 @@ export default class Home extends React.Component {
         <RoomSidebar serverName="default" {...this.context}/>
         <MessageContainer room={1} />
         <ChatInput {...this.context} />
-        <div className='user-sign-out'>
-          <span>{user.username}</span>
-          <Button onClick={handleSignOut}>sign out</Button>
-        </div>
       </>
     );
   }
